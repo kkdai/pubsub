a66a832ec57b81faa3867a85fcbde49d88f850dd
Evan Lin <evanslin@gmail.com>
2015-08-20 10:28:43 +0800

    Add more example

--- README.md
+++ README.md
@@ -40,6 +40,31 @@ Usage
         	//Got "test1"
         	fmt.Println(<-c2)
         	//Got "test2"
+
+
+            // Add subscription "topic2" for c1.          
+        	ser.AddSubscription(c1, "topic2")
+
+            // Publish new content in topic2
+        	ser.Publish("test3", "topic2")
+
+        	fmt.Println(<-c1)
+        	//Got "test3"
+        	
+            // Remove subscription "topic2" in c1
+        	ser.RemoveSubscription(c1, "topic2")
+        	
+            // Publish new content in topic2
+        	ser.Publish("test4", "topic2")
+        
+        	select {
+        	case val := <-c1:
+        		fmt.Printf("Should not get %v notify on remove topic\n", val)
+        		break
+        	case <-time.After(time.Second):
+        	    //Will go here, because we remove subscription topic2 in c1.         
+        		break
+        	}
         }
 
 
--- example_test.go
+++ example_test.go
@@ -2,6 +2,7 @@ package pubsub_test
 
 import (
 	"fmt"
+	"time"
 
 	. "github.com/kkdai/pubsub"
 )
@@ -10,21 +11,36 @@ func ExamplePubsub() {
 	ser := NewPubsub(1)
 	c1 := ser.Subscribe("topic1")
 	c2 := ser.Subscribe("topic2")
-
-	//Add subscription for c1
-	ser.AddSubscription(c1, "topic2", "topic3")
-	//Remove subscription topic2 in c1
-	ser.RemoveSubscription(c1, "topic2")
-
-	//Publish content "test1" in topic "topic1"
 	ser.Publish("test1", "topic1")
-	//Publish content "test2" in topic "topic2"
 	ser.Publish("test2", "topic2")
-
 	fmt.Println(<-c1)
 	//Got "test1"
 	fmt.Println(<-c2)
 	//Got "test2"
+
+	// Add subscription "topic2" for c1.
+	ser.AddSubscription(c1, "topic2")
+
+	// Publish new content in topic2
+	ser.Publish("test3", "topic2")
+
+	fmt.Println(<-c1)
+	//Got "test3"
+
+	// Remove subscription "topic2" in c1
+	ser.RemoveSubscription(c1, "topic2")
+
+	// Publish new content in topic2
+	ser.Publish("test4", "topic2")
+
+	select {
+	case val := <-c1:
+		fmt.Printf("Should not get %v notify on remove topic\n", val)
+		break
+	case <-time.After(time.Second):
+		//Will go here, because we remove subscription topic2 in c1.
+		break
+	}
 }
 
 func ExamplePubsub_Subscribe() {
